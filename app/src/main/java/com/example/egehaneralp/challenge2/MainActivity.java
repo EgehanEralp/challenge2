cb7cfca0fa5a0e7d89f66578316a5b0be0b129d1
Egehan Eralp <egehaneralp@icloud.com>
2019-01-26 00:24:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/egehaneralp/challenge2/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.egehaneralp.challenge2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tv=findViewById(R.id.tv);
+
+        Thread t=new Thread(){
+            @Override
+            public void run(){
+                while(!isInterrupted()){
+
+                    try {
+                        Thread.sleep(1000);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                count++;
+                                tv.setText(String.valueOf(count));
+                                if(count==15){
+
+                                    finish();
+                                    moveTaskToBack(true);
+                                }
+                            }
+                        });
+                        
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        };
+        t.start();
+
+
+    }
+}
